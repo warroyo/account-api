2b908733f5d0fd7f036817d4dab5be5df0a444cd
warroyo <warroyo7199008@gmail.com>
2019-01-02 17:25:25 -0700

    first push

---
+++ Controllers/AccountsController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace account_api.Controllers
+{
+    
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private IConfiguration configuration;
+        public AccountsController(IConfiguration iConfig)  
+        {  
+        configuration = iConfig;  
+        }   
+        // GET api/accounts
+        [HttpGet]
+        public IEnumerable<Account> Get()
+        {
+            string order_service = configuration.GetValue<string>("endpoints:order-service");  
+
+            using (AccountDb db = new AccountDb())
+            {
+                return db.Accounts.ToList();
+            }
+        }
+  
+
+        // GET api/accounts/5
+        [HttpGet("{id}")]
+        public Account Get(int id)
+        {
+            using (AccountDb db = new AccountDb())
+            {
+                return db.Accounts.First(t => t.Id == id);
+            }
+        }
+
+        // POST api/accounts
+        [HttpPost]
+        public void Post([FromBody]JObject value)
+        {
+            Account posted = value.ToObject<Account>();
+            using (AccountDb db = new AccountDb())
+            {
+                db.Accounts.Add(posted);
+                db.SaveChanges();
+            }
+        }
+
+        // PUT api/accounts/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]JObject value)
+        {
+            Account posted = value.ToObject<Account>();
+            posted.Id = id; // Ensure an id is attached
+            using (AccountDb db = new AccountDb())
+            {
+                db.Accounts.Update(posted);
+                db.SaveChanges();
+            }
+        }
+        // DELETE api/accounts/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            using (AccountDb db = new AccountDb())
+            {
+                if (db.Accounts.Where(t => t.Id == id).Count() > 0) // Check if element exists
+                    db.Accounts.Remove(db.Accounts.First(t => t.Id == id));
+                db.SaveChanges();
+            }
+        }
+    }
+}
---
+++ accountDb.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace account_api
+{
+    public class AccountDb : DbContext
+    {
+        public DbSet<Account> Accounts { get; set; }  
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Filename=./Accounts.db");
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.Entity<Account>().HasData(
+                new Account 
+                {
+                    Id = 1,
+                    First = "bob",
+                    Last = "smith",
+                    Email = "bob.smith@gmail.com"
+                },
+                new Account 
+                {
+                    Id = 2,
+                    First = "jim",
+                    Last = "roberts",
+                    Email = "jim.roberts@gmail.com"
+                },
+                new Account 
+                {
+                    Id = 3,
+                    First = "alison",
+                    Last = "jacobs",
+                    Email = "alison.jacobs@aol.com"
+                }
+            );
+        }
+    }
+}
