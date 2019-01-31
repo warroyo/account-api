893af21a952595ce1e343cd207ec6892746dc71d
warroyo <warroyo7199008@gmail.com>
2019-01-30 22:49:38 -0700

    change timeout

--- smoke/test.rb
+++ smoke/test.rb
@@ -1,5 +1,5 @@
 
 route = ENV["ROUTE"]
-describe http("#{route}/api/accounts", method: 'GET', open_timeout: 60, read_timeout: 60, ssl_verify: false) do
+describe http("#{route}/api/accounts", method: 'GET', open_timeout: 15, read_timeout: 15, ssl_verify: false) do
   its('status') { should eq 200 }
 end
